cad269937e8e30d222b18685b91fd2b38106726a
Alexander Bedulin <alexander.bedulin@gmail.com>
2019-09-09 11:45:26 +0300

    new android package support

--- Ziggeo.Xamarin.NetStandard.Demo.Android/MainActivity.cs
+++ Ziggeo.Xamarin.NetStandard.Demo.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Acr.UserDialogs;
 
 using Ziggeo;
 
@@ -18,8 +19,9 @@ namespace Ziggeo.Xamarin.NetStandard.Demo.Droid
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-
+               
             base.OnCreate(savedInstanceState);
+            UserDialogs.Init(this);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App(new ZiggeoApplication(App.ZiggeoApplicationToken)));
         }
