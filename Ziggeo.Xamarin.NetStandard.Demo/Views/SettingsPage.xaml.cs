190d5a3f00f0ffa6cfbcfaff39aab2725df708df
Alexander Bedulin <alexander.bedulin@gmail.com>
2020-09-01 15:39:20 +0300

    Added settings stub screen

---
+++ Ziggeo.Xamarin.NetStandard.Demo/Views/SettingsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Ziggeo.Xamarin.NetStandard.Demo.Views
+{
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
