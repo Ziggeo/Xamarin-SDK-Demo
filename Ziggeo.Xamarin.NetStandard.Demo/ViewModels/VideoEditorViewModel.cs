5dd150e88a9bbb281ecb39fe24b0f8b2426b5a91
Alexander Bedulin <alexander.bedulin@gmail.com>
2020-09-01 12:03:58 +0300

    Added video editor stub

---
+++ Ziggeo.Xamarin.NetStandard.Demo/ViewModels/VideoEditorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Ziggeo.Xamarin.NetStandard.Demo.ViewModels
+{
+    public class VideoEditorViewModel : BaseViewModel
+    {
+        public VideoEditorViewModel()
+        {
+        }
+    }
+}
