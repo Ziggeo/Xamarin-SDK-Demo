8d5d3f8c778cbfbf4b435770545cd2bd0f1b84e4
Alex Bedulin <alexander.bedulin@gmail.com>
2022-06-17 14:17:15 +0200

    fixes

--- Ziggeo.Xamarin.NetStandard.Demo.Android/MainActivity.cs
+++ Ziggeo.Xamarin.NetStandard.Demo.Android/MainActivity.cs
@@ -1,41 +1,35 @@
-﻿using Acr.UserDialogs;
+﻿using System;
+
 using Android.App;
 using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
 using Android.OS;
+using Acr.UserDialogs;
 using FFImageLoading.Forms.Platform;
-using Xamarin.Forms;
-using Xamarin.Forms.Platform.Android;
-using Platform = Xamarin.Essentials.Platform;
+using Xamarin.Essentials;
+
+using Ziggeo;
 
 namespace Ziggeo.Xamarin.NetStandard.Demo.Droid
 {
-    
-    // #define TEST 
-    
     [Activity(Label = "Ziggeo demo. Xamarin", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : FormsAppCompatActivity
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            // TabLayoutResource = Resource.Layout.Tabbar;
-            // ToolbarResource = Resource.Layout.Toolbar;
-
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+               
             base.OnCreate(savedInstanceState);
-
-// #if (DEBUG && TEST)
-           // Your TestConfiguration.cs Activity
-           // Intent iTestConfiguration = new Intent(this, typeof(TestConfiguration));
-           // StartActivity(iTestConfiguration);
-// #endif
-// #if !TEST
             Platform.Init(this, savedInstanceState);
-            Forms.Init(this, savedInstanceState);
-            FormsMaterial.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            global::Xamarin.Forms.FormsMaterial.Init(this, savedInstanceState);
             CachedImageRenderer.Init(true);
             CachedImageRenderer.InitImageViewHandler();
             UserDialogs.Init(this);
             LoadApplication(new App(new ZiggeoApplication()));
-// #endif
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
@@ -45,5 +39,4 @@ namespace Ziggeo.Xamarin.NetStandard.Demo.Droid
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
-
 }
