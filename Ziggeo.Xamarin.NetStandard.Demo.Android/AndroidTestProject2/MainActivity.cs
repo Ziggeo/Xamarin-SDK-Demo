0834194ab8868476d4015a25ca846a320deebafd
Alex Bedulin <alexander.bedulin@gmail.com>
2022-06-20 11:54:53 +0200

    test running

--- Ziggeo.Xamarin.NetStandard.Demo.Android/AndroidTestProject2/MainActivity.cs
+++ Ziggeo.Xamarin.NetStandard.Demo.Android/AndroidTestProject2/MainActivity.cs
@@ -1,22 +1,37 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using Android.App;
-using Android.OS;
+using Android.Runtime;
 using Xamarin.Android.NUnitLite;
 
 namespace AndroidTestProject2
 {
-    [Activity(Label = "AndroidTestProject2", MainLauncher = true)]
-    public class MainActivity : TestSuiteActivity
-    {
-        protected override void OnCreate(Bundle bundle)
-        {
-            // tests can be inside the main assembly
-            AddTest(Assembly.GetExecutingAssembly());
-            // or in any reference assemblies
-            // AddTest (typeof (Your.Library.TestClass).Assembly);
+    // [Activity(Label = "AndroidTestProject2", MainLauncher = true)]
+    // public class MainActivity : TestSuiteActivity
+    // {
+    //     protected override void OnCreate(Bundle bundle)
+    //     {
+    //         // tests can be inside the main assembly
+    //         AddTest(Assembly.GetExecutingAssembly());
+    //         // or in any reference assemblies
+    //         // AddTest (typeof (Your.Library.TestClass).Assembly);
+    //
+    //         // Once you called base.OnCreate(), you cannot add more assemblies.
+    //         base.OnCreate(bundle);
+    //     }
+    // }
 
-            // Once you called base.OnCreate(), you cannot add more assemblies.
-            base.OnCreate(bundle);
+
+    [Instrumentation(Name = "AndroidTestProject2.TestInstrumentation")]
+    public class TestInstrumentation : TestSuiteInstrumentation
+    {
+        public TestInstrumentation(IntPtr handle, JniHandleOwnership transfer) : base(handle, transfer)
+        {
+        }
+
+        protected override void AddTests()
+        {
+            AddTest(Assembly.GetExecutingAssembly());
         }
     }
 }
